9e0c486347d78a81aeccd4f37cba75bb89ac1e9c
Achile Egbunu <achilesolomon@gmail.com>
2024-01-05 21:18:57 -0600

    Add operate function

--- js/index.js
+++ js/index.js
@@ -3,6 +3,22 @@ let firstNumber;
 let secondNumber;
 let operator; // * +  - /
 
+// operate function
+function operate(firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+        default:
+            return undefined;
+    };
+}
+
 // add function
 function add(a, b) {
     return Number(a) + Number(b);
